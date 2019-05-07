722b718b45d808380bd4ddc0c962e4f74afbaa22
Yevhenii Ishchenko <yevhenii.i@hexagen48.com>
2019-05-07 23:54:18 +0200

    Define ViewModel class

--- workshop/src/main/java/io/swingdev/microconf/workshop/di/ViewModelModule.kt
+++ workshop/src/main/java/io/swingdev/microconf/workshop/di/ViewModelModule.kt
@@ -1,8 +1,12 @@
 package io.swingdev.microconf.workshop.di
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
+import io.swingdev.microconf.workshop.di.annotation.ViewModelKey
+import io.swingdev.microconf.workshop.presentation.main.CatFactsViewModel
 import io.swingdev.microconf.workshop.utils.ViewModelFactory
 
 @Module
@@ -11,5 +15,8 @@ interface ViewModelModule {
     @Binds
     fun bindsViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
 
-    // TODO: Bind CatFactViewModel
+    @Binds
+    @IntoMap
+    @ViewModelKey(CatFactsViewModel::class)
+    fun bindsCatFactsViewModel(catFactsViewModel: CatFactsViewModel): ViewModel
 }
--- workshop/src/main/java/io/swingdev/microconf/workshop/presentation/main/CatFactsViewModel.kt
+++ workshop/src/main/java/io/swingdev/microconf/workshop/presentation/main/CatFactsViewModel.kt
@@ -1,22 +1,38 @@
 package io.swingdev.microconf.workshop.presentation.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
+import io.swingdev.microconf.workshop.domain.CatFactsRepository
+import io.swingdev.microconf.workshop.domain.model.CatFact
+import javax.inject.Inject
 
-// TODO: Inject constructor
-class CatFactsViewModel constructor() : ViewModel() {
-    // TODO: Declare CatFacts list
-
-    // TODO: Declare Error Handler
+class CatFactsViewModel @Inject constructor(
+    private val repository: CatFactsRepository
+) : ViewModel() {
+    val catFacts = MutableLiveData<List<CatFact>>()
+    val errorHandler = MutableLiveData<Throwable>()
 
     private val compositeDisposable = CompositeDisposable()
 
     init {
-        // TODO: Fetch data from repository
+        repository.fetchCatFacts()
+            .subscribeBy(onError = {
+                errorHandler.postValue(it)
+            }, onNext = {
+                catFacts.postValue(it)
+            }).addTo(compositeDisposable)
     }
 
     fun updateData() {
-        // TODO: Update data from repository
+        repository.updateCatFacts()
+            .subscribeBy(onError = {
+                errorHandler.postValue(it)
+            }, onNext = {
+                catFacts.postValue(it)
+            }).addTo(compositeDisposable)
     }
 
     override fun onCleared() {
