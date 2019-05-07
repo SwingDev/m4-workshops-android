1dcf3b93a4706750fb3ad937db29b7ea7914cb24
Yevhenii Ishchenko <yevhenii.i@hexagen48.com>
2019-05-07 23:46:56 +0200

    Define Repository class

--- workshop/src/main/java/io/swingdev/microconf/workshop/domain/CatFactsRepository.kt
+++ workshop/src/main/java/io/swingdev/microconf/workshop/domain/CatFactsRepository.kt
@@ -1,19 +1,40 @@
 package io.swingdev.microconf.workshop.domain
 
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
 import io.swingdev.microconf.workshop.domain.db.CatFactsDao
+import io.swingdev.microconf.workshop.domain.model.CatFact
 import io.swingdev.microconf.workshop.domain.network.ApiClient
+import javax.inject.Inject
 
-// TODO: Inject constructor
-class CatFactsRepository constructor(
+class CatFactsRepository @Inject constructor(
     private val apiClient: ApiClient,
     private val catFactsDao: CatFactsDao
 ) {
 
-    // TODO: Invoke DAO and return Observable
-    fun fetchCatFacts() = Unit
+    fun fetchCatFacts(): Observable<List<CatFact>> =
+        catFactsDao.getAll()
+            .observeOn(Schedulers.io())
+            .subscribeOn(Schedulers.io())
+            .defaultIfEmpty(emptyList())
+            .toObservable()
+            .flatMap { savedFacts ->
+                when {
+                    savedFacts.isEmpty() -> updateCatFacts()
+                    else -> Observable.just(savedFacts)
+                }
+            }
 
-    // TODO: Invoke API Client and return Observable
-    fun updateCatFacts() = Unit
+    fun updateCatFacts(): Observable<List<CatFact>> =
+        apiClient.getCatFacts(DEFAULT_AMOUNT)
+            .observeOn(Schedulers.io())
+            .subscribeOn(Schedulers.io())
+            .toObservable()
+            .map { receivedFacts ->
+                catFactsDao.clearAll()
+                catFactsDao.saveAll(receivedFacts)
+                receivedFacts
+            }
 
     companion object {
         private const val DEFAULT_AMOUNT = 20
